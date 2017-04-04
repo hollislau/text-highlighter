768316c4d3ac96e9e937c15dd3b53fa0d7da68b2
Hollis Lau <hollislau@hotmail.com>
2017-04-04 02:27:57 -0700

    Add Webpack and CSS build tasks to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,27 @@ const webpackStream = require('webpack-stream');
 
 var nodemonStream;
 
-const lintFiles = [
-  'app/**/*.js',
-  '*.js'
-];
-const staticFiles = ['app/**/*.html', 'app/**/*.css'];
+const lintFiles = ['app/**/*.js', '*.js'];
+const buildFiles = ['babel-polyfill', 'app/js/entry.js'];
+const webpackOptions = {
+  output: {
+    filename: 'main.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: [
+          __dirname + '/app/js'
+        ],
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
 
 gulp.task('lint', () => {
   return gulp.src(lintFiles)
@@ -22,13 +38,43 @@ gulp.task('lint', () => {
 });
 
 gulp.task('static:dev', () => {
-  return gulp.src(staticFiles)
+  return gulp.src('app/index.html')
+    .pipe(gulp.dest('build'))
+    .pipe(livereload());
+});
+
+gulp.task('css:dev', () => {
+  return gulp.src('app/css/main.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('build'))
+    .pipe(livereload());
+});
+
+gulp.task('webpack:dev', () => {
+  return gulp.src(buildFiles)
+    .pipe(webpackStream(webpackOptions, webpack))
     .pipe(gulp.dest('build'))
     .pipe(livereload());
 });
 
 gulp.task('static:pro', () => {
-  return gulp.src(staticFiles)
+  return gulp.src('app/index.html')
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('css:pro', () => {
+  return gulp.src('app/css/main.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('webpack:pro', () => {
+  return gulp.src(buildFiles)
+    .pipe(webpackStream(webpackOptions, webpack))
     .pipe(gulp.dest('build'));
 });
 
@@ -49,10 +95,12 @@ gulp.task('watch', ['build:dev'], () => {
   }
 
   livereload.listen();
-  gulp.watch(staticFiles, ['static:dev']);
+  gulp.watch('app/index.html', ['static:dev']);
+  gulp.watch('app/css/main.css', ['css:dev']);
+  gulp.watch('app/**/*.js', ['webpack:dev']);
   gulp.watch('gulpfile.js', ['watch']);
 });
 
-gulp.task('build:dev', ['static:dev']);
-gulp.task('build:pro', ['static:pro']);
+gulp.task('build:dev', ['webpack:dev', 'css:dev', 'static:dev']);
+gulp.task('build:pro', ['webpack:pro', 'css:pro', 'static:pro']);
 gulp.task('default', ['lint']);
