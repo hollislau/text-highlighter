7adafd987b6eff836b996e41d1869254f13703ac
Hollis Lau <hollislau@hotmail.com>
2017-04-06 02:56:14 -0700

    Add support to include capitalization and punctuation; fix bugs

--- app/js/entry.js
+++ app/js/entry.js
@@ -7,7 +7,7 @@ const wordList = [
   },
   {
     color: 'green',
-    words: ['adorable', 'creative', 'love', 'new technology']
+    words: ['adorable', 'creative', 'love', 'new technology', 'groundbreaking']
   },
   {
     color: 'blue',
@@ -25,10 +25,13 @@ const wordList = [
 const divOne = document.getElementById('container-one');
 const divTwo = document.getElementById('container-two');
 const divThree = document.getElementById('container-three');
+const divFour = document.getElementById('container-four');
 const highlighterOne = highlighter(divOne, wordList);
 const highlighterTwo = highlighter(divTwo, wordList);
 const highlighterThree = highlighter(divThree, wordList);
+const highlighterFour = highlighter(divFour, wordList);
 
-highlighterOne.highlight('First string with candidates, love, and candidates in the middle.');
-highlighterTwo.highlight('Second string with an arm, radio, radio, and will deliver new in the middle.');
-highlighterThree.highlight('No matches here.');
+highlighterOne.highlight('Aggressive string with a radio, love, a log file, and another radio is creative!');
+highlighterTwo.highlight('I do not want new technology; our team will deliver new, action-oriented candidates. Also, an army of farm animals is alarming but charming, if you have a strong arm?');
+highlighterThree.highlight('Nothing to see here.');
+highlighterFour.highlight('We expect our candidates to be action-oriented, aggressive and have creative ideas for our team. You will deliver new technology and groundbreaking designs.');
--- app/js/highlighter.js
+++ app/js/highlighter.js
@@ -1,31 +1,38 @@
 module.exports = function (div, wordList) {
   const highlighter = {
     highlight: function (str) {
-      var newP = document.createElement('p');
-      var foundArr = [];
-      var endStrIndex;
-      var endStr;
-      var endPText;
+      const newP = document.createElement('p');
+      const lowerCase = str.toLowerCase();
+      const foundArr = [];
+
+      var newPText;
 
       div.appendChild(newP);
 
       for (let i = 0; i < wordList.length; i++) {
         for (let j = 0; j < wordList[i].words.length; j++) {
           let currentWord = wordList[i].words[j];
-          let foundIndex = str.indexOf(currentWord);
+          let foundIndex = lowerCase.indexOf(currentWord);
 
           while (foundIndex > -1) {
-            let endIndex = foundIndex + currentWord.length;
+            let endIndex = foundIndex + currentWord.length - 1;
 
-            foundArr.push({
-              word: currentWord,
-              startIndex: foundIndex,
-              endIndex: endIndex,
-              color: wordList[i].color,
-              priority: wordList[i]
-            });
+            if (lowerCase[foundIndex - 1] === ' ' || !lowerCase[foundIndex - 1]) {
+              if (lowerCase[endIndex + 1] && lowerCase[endIndex + 1].search(/[,.:;!?]/) > -1) {
+                endIndex++;
+              }
 
-            foundIndex = str.indexOf(currentWord, endIndex);
+              if (lowerCase[endIndex + 1] === ' ' || !lowerCase[endIndex + 1]) {
+                foundArr.push({
+                  startIndex: foundIndex,
+                  endIndex: endIndex,
+                  color: wordList[i].color,
+                  priority: wordList[i]
+                });
+              }
+            }
+
+            foundIndex = lowerCase.indexOf(currentWord, endIndex);
           }
         }
       }
@@ -35,10 +42,11 @@ module.exports = function (div, wordList) {
       });
 
       for (let i = 0; i < foundArr.length; i++) {
-        let leadStrIndex = foundArr[i - 1] ? foundArr[i - 1].endIndex : 0;
+        let leadStrIndex = foundArr[i - 1] ? foundArr[i - 1].endIndex + 1 : 0;
         let leadStr = str.slice(leadStrIndex, foundArr[i].startIndex);
+        let foundStr = str.slice(foundArr[i].startIndex, foundArr[i].endIndex + 1);
         let newPText = document.createTextNode(leadStr);
-        let newSpanText = document.createTextNode(foundArr[i].word);
+        let newSpanText = document.createTextNode(foundStr);
         let newSpan = document.createElement('span');
 
         newP.appendChild(newPText);
@@ -47,10 +55,16 @@ module.exports = function (div, wordList) {
         newP.appendChild(newSpan);
       }
 
-      endStrIndex = foundArr.length ? foundArr[foundArr.length - 1].endIndex : 0;
-      endStr = str.slice(endStrIndex);
-      endPText = document.createTextNode(endStr);
-      div.firstChild.appendChild(endPText);
+      if (foundArr.length) {
+        let lastStrIndex = foundArr[foundArr.length - 1].endIndex + 1;
+        let lastStr = str.slice(lastStrIndex);
+
+        newPText = document.createTextNode(lastStr);
+      } else {
+        newPText = document.createTextNode(str);
+      }
+
+      newP.appendChild(newPText);
     }
   };
 
