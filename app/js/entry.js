b28b905f0f01e23b242223b248bf75718dddb42e
Hollis Lau <hollislau@hotmail.com>
2017-04-07 00:55:19 -0700

    Add working test for overlapping hover

--- app/js/entry.js
+++ app/js/entry.js
@@ -26,12 +26,34 @@ const divOne = document.getElementById('container-one');
 const divTwo = document.getElementById('container-two');
 const divThree = document.getElementById('container-three');
 const divFour = document.getElementById('container-four');
+const testParent = document.getElementById('test-parent');
+const test = document.getElementById('test');
+const altParent = document.getElementById('alt-parent');
 const highlighterOne = highlighter(divOne, wordList);
 const highlighterTwo = highlighter(divTwo, wordList);
 const highlighterThree = highlighter(divThree, wordList);
 const highlighterFour = highlighter(divFour, wordList);
 
-highlighterOne.highlight('Aggressive string with a radio, love, a log file, and another radio is creative! An adorable puppy is super adorable.');
-highlighterTwo.highlight('I do not want new technology; our team will deliver new, action-oriented candidates. Also, an army of farm animals is alarming but charming, if you have a strong arm?');
+highlighterOne.highlight('Do  not   cross my radio (love my radio!) with a log file! While creative, I do not want new technology. An adorable puppy is super adorable.');
+highlighterTwo.highlight('Our team is very unlikely to leave behind promising candidates; we will deliver new, action-oriented prospects. Also, an army of farm animals is alarming but charming, if you have an aggressive arm?');
 highlighterThree.highlight('Nothing to see here.');
 highlighterFour.highlight('We expect our candidates to be action-oriented, aggressive and have creative ideas for our team. You will deliver new technology and groundbreaking designs.');
+
+test.addEventListener('mouseover', (e) => {
+  e.stopPropagation();
+  testParent.removeAttribute('class');
+});
+
+test.addEventListener('mouseout', () => {
+  testParent.className = 'red';
+});
+
+testParent.addEventListener('mouseover', () => {
+  testParent.className = 'hidden';
+  altParent.removeAttribute('class');
+});
+
+altParent.addEventListener('mouseout', () => {
+  altParent.className = 'hidden';
+  testParent.className = 'red';
+});
--- app/js/highlighter.js
+++ app/js/highlighter.js
@@ -1,8 +1,9 @@
 module.exports = function (div, wordList) {
   const highlighter = {
     highlight: function (str) {
+      const formatted = str.replace(/\s\s+/g, ' ');
+      const lowerCase = formatted.toLowerCase();
       const newP = document.createElement('p');
-      const lowerCase = str.toLowerCase();
       const foundArr = [];
 
       var newPText;
@@ -15,24 +16,28 @@ module.exports = function (div, wordList) {
           let foundIndex = lowerCase.indexOf(currentWord);
 
           while (foundIndex > -1) {
-            let endIndex = foundIndex + currentWord.length - 1;
+            let wordEndIndex = foundIndex + currentWord.length - 1;
+            let preWordChar = lowerCase[foundIndex - 1];
 
-            if (lowerCase[foundIndex - 1] === ' ' || !lowerCase[foundIndex - 1]) {
-              if (lowerCase[endIndex + 1] && lowerCase[endIndex + 1].search(/[,.:;!?]/) > -1) {
-                endIndex++;
+            if (preWordChar === ' ' || preWordChar === '(' || !preWordChar) {
+              let postWordChar = lowerCase[wordEndIndex + 1];
+
+              if (postWordChar && postWordChar.search(/[,.:;!?]/) > -1) {
+                wordEndIndex++;
+                postWordChar = lowerCase[wordEndIndex + 1];
               }
 
-              if (lowerCase[endIndex + 1] === ' ' || !lowerCase[endIndex + 1]) {
+              if (postWordChar === ' ' || postWordChar === ')' || !postWordChar) {
                 foundArr.push({
                   startIndex: foundIndex,
-                  endIndex: endIndex,
+                  endIndex: wordEndIndex,
                   color: wordList[i].color,
                   priority: wordList[i]
                 });
               }
             }
 
-            foundIndex = lowerCase.indexOf(currentWord, endIndex);
+            foundIndex = lowerCase.indexOf(currentWord, wordEndIndex + 1);
           }
         }
       }
@@ -43,25 +48,25 @@ module.exports = function (div, wordList) {
 
       for (let i = 0; i < foundArr.length; i++) {
         let leadStrIndex = foundArr[i - 1] ? foundArr[i - 1].endIndex + 1 : 0;
-        let leadStr = str.slice(leadStrIndex, foundArr[i].startIndex);
-        let foundStr = str.slice(foundArr[i].startIndex, foundArr[i].endIndex + 1);
+        let leadStr = formatted.slice(leadStrIndex, foundArr[i].startIndex);
+        let foundStr = formatted.slice(foundArr[i].startIndex, foundArr[i].endIndex + 1);
         let newPText = document.createTextNode(leadStr);
         let newSpanText = document.createTextNode(foundStr);
         let newSpan = document.createElement('span');
 
         newP.appendChild(newPText);
         newSpan.appendChild(newSpanText);
-        newSpan.className = foundArr[i].color;
+        newSpan.className = foundArr[i].color + ' hover';
         newP.appendChild(newSpan);
       }
 
       if (foundArr.length) {
         let lastStrIndex = foundArr[foundArr.length - 1].endIndex + 1;
-        let lastStr = str.slice(lastStrIndex);
+        let lastStr = formatted.slice(lastStrIndex);
 
         newPText = document.createTextNode(lastStr);
       } else {
-        newPText = document.createTextNode(str);
+        newPText = document.createTextNode(formatted);
       }
 
       newP.appendChild(newPText);
